7af0861472eb89406dc5e3a1c5b592d2c051cc97
k2a0 <kirillacharya7@gmail.com>
2024-11-15 00:14:47 +0100

    The initial template for parsing movie metadata

---
+++ src/scripts/parse_movie_metadata.py
@@ -0,0 +1,36 @@
+import wikipediaapi
+
+wiki = wikipediaapi.Wikipedia('en')
+
+def get_movie_data(wikipedia_movie_id):
+    page = wiki.page(wikipedia_movie_id)
+
+    if not page.exists():
+        return None
+
+    data = {
+        "wikipedia_movie_id": wikipedia_movie_id,
+        "release_date": None,
+        "plot_summary": None,
+        "genres": None,
+        "keywords": None,
+        "cast": None
+    }
+
+    for section in page.sections:
+        if 'release' in section.title.lower():
+            data["release_date"] = section.text
+
+        if 'plot' in section.title.lower():
+            data["plot_summary"] = section.text
+
+        if 'genre' in section.title.lower():
+            data["genres"] = section.text
+
+        if 'keywords' in section.title.lower():
+            data["keywords"] = section.text
+
+        if 'cast' in section.title.lower():
+            data["cast"] = section.text
+
+    return data
