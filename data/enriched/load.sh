d833fa8b677ee22e3dabf1f2d8fc84fc98e84b08
alexdremov <dremov.me@gmail.com>
2024-12-18 20:37:40 +0100

    infobox actors

---
+++ data/enriched/load.sh
@@ -0,0 +1,5 @@
+wget -O actors/actors_freebase.csv \
+    https://drive.usercontent.google.com/download?id=1l-vkiXnDV4Y3JqqWVeeqdEuTLz3bCxy9&export=download&authuser=1
+
+wget -O actors/actors_freebase_wiki.csv \
+    https://drive.usercontent.google.com/download?id=1LJ2LAb5eknkKm23ivO9Izy4mAJ07t_05&export=download&authuser=1
